d184847bbf3200e432945217c042dc832d456d4f
gaelTorrecillas <gael.torrecillas@mind-and-go.com>
2023-10-20 17:28:49 +0200

    add version 16

--- src/main/java/com/odoojava/api/Session.java
+++ src/main/java/com/odoojava/api/Session.java
@@ -183,8 +183,8 @@ public class Session {
 
 	private void checkVersionCompatibility() throws XmlRpcException, OdooApiException {
 
-		if (this.getServerVersion().getMajor() < 8 || this.getServerVersion().getMajor() > 15) {
-			throw new OdooApiException("Only Odoo Version from v8.x to 15.x are maintained. "
+		if (this.getServerVersion().getMajor() < 8 || this.getServerVersion().getMajor() > 16) {
+			throw new OdooApiException("Only Odoo Version from v8.x to 16.x are maintained. "
 					+ "Please choose another version of the library");
 		}
 
